56bf3fb657491f09b12fa130cb232e67135f630f
khajathoufeek <khajathoufeek.a@gmail.com>
2024-06-15 07:57:47 +0530

    Update P1.java
    checking the edit option

--- src/git/org/P1.java
+++ src/git/org/P1.java
@@ -2,7 +2,8 @@ package git.org;
 
 public class P1 {
 public static void main(String[] args) {
-	System.out.println("waiting");
+	System.out.println("waiting for new update");
+	System.out.println("checking whether its working");
 }
 }
 
